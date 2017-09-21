3b42d48dfa8993bd1ef9b1c2a8159334b6dd52f5
yanbing2020 <374726241@qq.com>
2017-09-20 22:13:00 -0500

    增加程序文件

---
+++ division.py
@@ -0,0 +1,42 @@
+# _*_coding_*_ utf-8
+# __date__ = '17-9-7 下午2:04'
+
+import os
+import time
+
+
+def mkSubFile(lines, head, srcName, sub):
+    [des_filename, extname] = os.path.splitext(srcName)
+    filename = des_filename + '_' + str(sub) + extname
+    print('make file: %s' % filename)
+    fout = open(filename, 'w')
+    try:
+        fout.writelines([head])
+        fout.writelines(lines)
+        return sub + 1
+    finally:
+        fout.close()
+
+
+def splitByLineCount(filename, count):
+    fin = open(filename, 'r')
+    try:
+        head = fin.readline()
+        buf = []
+        sub = 1
+        for line in fin:
+            buf.append(line)
+            if len(buf) == count:
+                sub = mkSubFile(buf, head, filename, sub)
+                buf = []
+        if len(buf) != 0:
+            sub = mkSubFile(buf, head, filename, sub)
+    finally:
+        fin.close()
+
+
+if __name__ == '__main__':
+    begin = time.time()
+    splitByLineCount('/run/media/abin/备份专用盘/以入库数据/20170908/QQqun1/qun1.csv', 800000)
+    end = time.time()
+    print('time is %d seconds ' % (end - begin))
